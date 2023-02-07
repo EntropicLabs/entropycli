e027820b1476ed25742ce34148aa7fe6f6067e4f
Amit Prasad <mail@amitprasad.dev>
2023-02-07 16:39:43 -0500

    v1.0.9: Configurable LCD timeouts

--- src/cosmos/lcd.rs
+++ src/cosmos/lcd.rs
@@ -3,8 +3,12 @@ use super::network::Network;
 impl Network {
     pub async fn get(&self, path: &str) -> Result<reqwest::Response, reqwest::Error> {
         let url = format!("{}/{}", self.lcd_url, path);
+        let timeout = std::env::var("LCD_TIMEOUT")
+            .unwrap_or_else(|_| "30".to_string())
+            .parse::<u64>()
+            .unwrap_or(30);
         reqwest::ClientBuilder::new()
-            .timeout(std::time::Duration::from_secs(10))
+            .timeout(std::time::Duration::from_secs(timeout))
             .build()?
             .get(&url)
             .send()
@@ -17,8 +21,12 @@ impl Network {
         body: &serde_json::Value,
     ) -> Result<reqwest::Response, reqwest::Error> {
         let url = format!("{}/{}", self.lcd_url, path);
+        let timeout = std::env::var("LCD_TIMEOUT")
+            .unwrap_or_else(|_| "30".to_string())
+            .parse::<u64>()
+            .unwrap_or(30);
         reqwest::ClientBuilder::new()
-            .timeout(std::time::Duration::from_secs(10))
+            .timeout(std::time::Duration::from_secs(timeout))
             .build()?
             .post(&url)
             .json(body)
